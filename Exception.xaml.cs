2eba50996a5606b4f36c74753f6f46d40bc6305c
Николаев Андрей <Abramson21@mail.ru>
2020-09-29 14:52:10 +0300

    Добавлены итоговые файлы

---
+++ Exception.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eMailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для Exception.xaml
+    /// </summary>
+    public partial class Exception : Window
+    {
+        private string newError;
+
+        public string NewError
+        {
+            get
+            {
+                return newError;
+            }
+            set
+            {
+                newError = value;
+            }
+        }
+
+        public Exception(string newError)
+        {
+            this.newError = newError;
+            InitializeComponent();
+        }
+
+        
+
+        public void exError()
+        {
+            exception_value.Content = newError;
+        }
+    }
+}
---
+++ Library/Users/BaseUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace eMailSender.Library.Users
+{
+    class BaseUser
+    {
+        private string eMail;
+        private string nameUser;
+        public BaseUser(string eMail, string nameUser)
+        {
+            this.eMail = eMail;
+            this.nameUser = nameUser;
+        }
+
+        public MailAddress GetInfoUser()
+        {
+            return new MailAddress(eMail, nameUser);
+        }
+
+
+    }
+}
