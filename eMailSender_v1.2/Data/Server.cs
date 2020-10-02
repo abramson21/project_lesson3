98f991ebdf172735c7e22e6366e7488fc6adf64f
Николаев Андрей <Abramson21@mail.ru>
2020-10-02 12:17:59 +0300

    Add data

---
+++ eMailSender_v1.2/Data/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Documents;
+
+namespace eMailSender_v1._2.Data
+{
+    class Server
+    {
+        private int _Port;
+
+        public string Address { get; set; }
+        
+        public int Port
+        {
+            get => _Port;
+            set
+            {
+                if (value < 0 || value >= 65335)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапазоне от 0 до 65335");
+                _Port = value;
+            }
+        }
+
+        
+
+    }
+}
---
+++ eMailSender_v1.2/Data/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eMailSender_v1._2.Data
+{
+    class User
+    {
+    }
+}
